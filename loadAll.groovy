28838c982ffa834e63ace7e4724efd03a3f870fa
Kevin Harrington <mad.hephaestus@gmail.com>
2020-01-31 14:34:42 -0500

    updated hotfix

--- loadAll.groovy
+++ loadAll.groovy
@@ -1,4 +1,17 @@
+import com.neuronrobotics.bowlerstudio.scripting.ScriptingEngine
+import com.neuronrobotics.sdk.addons.kinematics.INewLinkProvider
+import com.neuronrobotics.sdk.addons.kinematics.LinkFactory
+
 HashMap<String, HashMap<String, Object>> database = ScriptingEngine.gitScriptRun(
 											"https://github.com/CommonWealthRobotics/DeviceProviders.git",
 											"devices.json",null)
-println 	database										
+for(String key:database.keySet()) {
+	HashMap<String, Object> data = database.get(key)
+	def provider = ScriptingEngine.gitScriptRun(
+											data.scriptGit,
+											data.scriptFile,[key])
+	if(INewLinkProvider.class.isInstance(provider)) {
+		println "Adding a link provider "+key+": "+data.description
+		LinkFactory.addLinkProvider(key, provider)
+	}
+}										
