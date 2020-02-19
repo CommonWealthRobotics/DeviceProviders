6ce564bf583d07a56f6005ef1339a4d2198b15f9
Kevin Harrington <mad.hephaestus@gmail.com>
2020-02-19 09:37:30 -0500

    adjusting the link loaders

--- loadAll.groovy
+++ loadAll.groovy
@@ -7,18 +7,20 @@ HashMap<String, HashMap<String, Object>> database = ScriptingEngine.gitScriptRun
 											"https://github.com/CommonWealthRobotics/DeviceProviders.git",
 											"devices.json",null)
 for(String key:database.keySet()) {
-	HashMap<String, Object> data = database.get(key)
+	//println "\n\nKey "+key
+	
 	try{
+		HashMap<String, Object> data = database.get(key)
+		//println "Loading "+data.description
 		ScriptingEngine.pull(data.scriptGit);
-		def provider = ScriptingEngine.gitScriptRun(
-												data.scriptGit,
-												data.scriptFile,[key])
+		def provider = ScriptingEngine.gitScriptRun(data.scriptGit,data.scriptFile,[key])
 		if(INewLinkProvider.class.isInstance(provider)) {
 			println "Adding a link provider "+key+": "+data.description
 			LinkFactory.addLinkProvider(key, provider)
 		}
 	}catch (Throwable t){
-		t.printStackTrace()
-		//BowlerStudio.printStackTrace(t)
+		//t.printStackTrace()
+		println "\n\nKey "+key
+		BowlerStudio.printStackTrace(t)
 	}
 }										
